33d93d1396fd6c3f77da263c15d436a5e1555eff
0520MZ <matthewjzhu0520@gmail.com>
2024-11-25 22:41:43 -0500

    Initial commit

---
+++ src/main/java/mzatthew/modid/TestModClient.java
@@ -0,0 +1,10 @@
+package mzatthew.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
